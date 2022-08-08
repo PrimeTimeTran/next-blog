769d9c12af0acc9b064e74b442c11f0961d8b03d
Loi Van Tran <datloiboi@gmail.com>
2022-08-08 09:05:35 -0400

    Update metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A tech blog for progeny',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://loi-tran-blog.netlify.app/',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
