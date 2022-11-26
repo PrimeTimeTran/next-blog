9085f75b4b7d490b00c9cdee3a323422f7833ad4
Loi Van Tran <datloiboi@gmail.com>
2022-11-25 22:15:48 -0500

    WIP: Lesson plans

---
+++ lessons/python/1-strings.py
@@ -0,0 +1,40 @@
+"Hello World"
+
+print("Hello World")
+
+print('Hello World')
+
+print('Hello World\'s')
+
+print("Hello World's")
+
+print("Hello \"YouTube\"")
+
+name = 'Harry'
+
+print(name)
+
+print(name + ' is magical.')
+
+print(f'{name} knows magic.')
+
+name2 = 'Ron'
+
+print(f'{name} and {name2} are wizards.')
+
+print('%s and %s attend Hogwarts.' % (name, name2))
+
+print('{} and {} know magic.'.format(name, name2))
+
+print(len(name))
+print(name.index('H'))
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
+
+phrase = "the fox jumped over the lazy dog"
+
+print(phrase.find('fox'))
+print(phrase[4:7])
+
+print(phrase.replace('fox', 'wizard'))
