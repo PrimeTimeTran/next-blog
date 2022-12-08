1640dc6a602b44d392062459c0eef285a6480865
Loi Van Tran <datloiboi@gmail.com>
2022-12-08 05:47:01 -0500

    WIP: Python

--- lessons/python/flow control.py
+++ lessons/python/flow control.py
@@ -1,17 +1,57 @@
-value = False
+value = True
 
 if value:
   print('True')
 
 if value:
-  print('Was True')
-else:
-  print('Was False')
-
-
-if value == True:
   print('True')
-elif value == True:
-  print('True 2')
 else:
   print('False')
+
+if not value:
+  print('True')
+else:
+  print('False')
+
+expression = 'spam'
+
+if expression == True:
+  print('True')
+elif expression == False:
+  print('False')
+else:
+  print('Is kinda like ham')
+
+print('0 < 10 is True') if 0 < 10 else print('0 < 10 is False')
+
+result = print('0 < 10 is True ') if 0 > 10 else '0 > 10 is False'
+print(result)
+
+
+if 1 > 0 and 10 > 0:
+  print('True')
+
+if 1 > 0 and -10 > 0:
+  print('True')
+
+if 1 > 0 or -10 > 0:
+  print('True')
+
+if 1 == 1:
+  print(True)
+
+one = 1
+
+if one is 1:
+  print(True)
+
+if 'a' in 'alphabet':
+  print('True')
+
+if 'a' in ['a', 'b']:
+  print('True')
+
+if 'z' not in 'alphabet':
+  print('🤯')
+  print('🤔')
+
