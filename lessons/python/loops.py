367d316666a6a7f39a08d229aba59a22f763ac80
Loi Van Tran <datloiboi@gmail.com>
2022-12-08 05:14:49 -0500

    WIP: Python

--- lessons/python/loops.py
+++ lessons/python/loops.py
@@ -1,7 +1,7 @@
 wizards = ['Harry', 'Ron', 'Hermione']
 
-for w in wizards:
-  print(w)
+for wizard in wizards:
+  print(wizard)
 
 for i, w in enumerate(wizards):
   print(i, w) 
@@ -21,5 +21,32 @@ while len(wizards) > 0:
   print(wizards)
   wizards.pop()
 
+for c in "Alphabet":
+  print(c)
 
+for i, c in enumerate("Alphabet"):
+  print(i, c)
 
+for n in set([1, 2, 3]):
+  print(n)
+
+states = {
+  "FL": "Florida",
+  "CA": "California",
+  "AK": "Alaska"
+}
+
+for s in states:
+  print(s)
+
+for s in states.values():
+  print(s)
+
+for s in states.items():
+  print(s)
+
+for k, v in states.items():
+  print(k, v)
+
+for i, [k,v] in enumerate(states.items()):
+  print(i, k, v)
--- lessons/python/variables.py
+++ lessons/python/variables.py
@@ -4,15 +4,17 @@ domain = '.com'
 
 print(protocol+website+domain)
 
-
 two = 2
 five = 5
 
-print( five + two)
-print( five - two)
-print( five * five)
-print( five / five)
-print( five // five)
-print( five ** five)
-print( five % two)
+print(five + two)
+print(five - two)
+print(five * five)
+print(five / five)
+print(five // five)
+print(five ** five)
+print(five % two)
+one, ten = 1, 10
+print(one, ten)
 
+print(ten*ten)
