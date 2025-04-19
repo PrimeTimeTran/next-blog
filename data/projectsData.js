6a4643f48fda1e0d47b0517c4336685c3b7beca2
PrimeTimeTran <primetimetran@gmail.com>
2025-04-19 10:37:55 +0700

    Update project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,25 +1,26 @@
 const projectsData = [
   {
-    title: `Seep Deep`,
-    description: `A LeetCode like platform for mastering DSAs, Databases, & Calculus`,
+    title: 'Seep Deep',
+    description: 'A LeetCode like platform for mastering DSAs, Databases, & Calculus',
     imgSrc: '/static/images/drone.jpeg',
     href: 'https://www.seepdeep.com/maths',
   },
   {
     title: 'Turboship',
-    description: `A scaffolding tool for generating NuxtJS, React Native, & Flutter code complete with models, APIs, & more after defining resources/models/entities.`,
+    description:
+      'A scaffolding tool for generating NuxtJS, React Native, & Flutter code complete with models, APIs, & more after defining resources/models/entities.',
     imgSrc: '/static/images/tiktok.jpeg',
     href: 'https://turboship.netlify.app',
   },
   {
     title: "World's Greatest Todo App",
-    description: `Building the world famous todo app using ReactJS, Firebase, Firestore, useHooks, and more.`,
+    description: 'Todo app built with ReactJS & Firebase',
     imgSrc: '/static/images/todo.jpeg',
-    href: 'https://todolistwithfirebase.netlify.app/',
+    href: 'https://todolistwithfirebase.netlify.app',
   },
   {
-    title: `Animations`,
-    description: `Build beautiful web pages using just HTML, CSS, JS, & Canvas`,
+    title: 'Animations',
+    description: 'Build beautiful web pages using just HTML, CSS, JS, & Canvas',
     imgSrc: '/static/images/illustrator.jpeg',
     href: 'https://cs-animations.netlify.app/',
   },
