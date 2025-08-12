9e1c4b13d15bb7ba85a6771fc1812feb0097f1a5
PrimeTimeTran <primetimetran@gmail.com>
2025-08-12 21:48:56 +0700

    Update proejects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,7 +5,16 @@ const projectsData = [
       'SeepDeep is the best platform to help you enhance your skills, expand your knowledge and prepare for technical interviews. Create Account. Start Exploring.',
     imgSrc: '/static/images/seepdeep.png',
     href: 'https://www.seepdeep.com',
-    tech: ['Flutter', 'NuxtJS', 'MongoDB', 'Docker', 'Netlify', 'GCP', 'Github Actions'],
+    tech: [
+      'Flutter',
+      'Material',
+      'NuxtJS',
+      'MongoDB',
+      'Docker',
+      'Netlify',
+      'GCP',
+      'Github Actions',
+    ],
   },
   {
     title: 'BakeSuite',
@@ -13,7 +22,7 @@ const projectsData = [
       'BakeSuite is an online web application to help cake designers, decorators, bakery owners, and home bakers to manage their day-to-day tasks.',
     imgSrc: '/static/images/bakesuite.png',
     href: 'https://baskesuite-demo.vercel.app',
-    tech: ['NextJS', 'Supabase', 'Vercel', 'GCP', 'Stripe', 'TailwindCSS'],
+    tech: ['ReactJS', 'NextJS', 'Supabase', 'Vercel', 'GCP', 'Stripe', 'TailwindCSS'],
   },
   {
     title: 'TurboShip',
@@ -21,7 +30,7 @@ const projectsData = [
       'Generate a professional codebase for your next multi-tenant SaaS app using NuxtJS 14, MongoDB, TailwindCSS, end-to-end testing, and more.',
     imgSrc: '/static/images/turboship.png',
     href: 'https://turboship.netlify.app',
-    tech: ['NuxtJS', 'MongoDB', 'Netlify', 'GCP', 'TailwindCSS'],
+    tech: ['VueJS', 'NuxtJS', 'MongoDB', 'Netlify', 'GCP', 'TailwindCSS'],
   },
   {
     title: 'RoyalStockExchange',
@@ -29,7 +38,7 @@ const projectsData = [
       'Trade stocks on RoyalStockExchange with commission-free investing & advanced trading tools. Access fractional shares, real-time market data, and more.',
     imgSrc: '/static/images/rse.png',
     href: 'https://royalstockexchange.com',
-    tech: ['Flutter', '.Net', 'Netlify', 'Entity Framework', 'GCP'],
+    tech: ['Flutter', 'Material', '.Net', 'Netlify', 'Entity Framework', 'GCP'],
   },
 ]
 
