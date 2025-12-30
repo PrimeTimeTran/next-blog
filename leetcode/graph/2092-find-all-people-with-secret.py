efdf9a35917a3d7c5c67709b160c04fe07e291ff
PrimeTimeTran <primetimetran@gmail.com>
2025-12-30 20:00:31 +0700

    Update problems

--- leetcode/graph/2092-find-all-people-with-secret.py
+++ leetcode/graph/2092-find-all-people-with-secret.py
@@ -21,27 +21,27 @@ This preserves simultaneity correctly.
 '''
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        parent = list(range(n))
+        i, parent = 0, list(range(n))
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
-        def union(x, y):
+        def union(x, y): 
             parent[find(x)] = find(y)
-        union(0, firstPerson)
         meetings.sort(key=lambda x: x[2])
-        i = 0
+        union(0, firstPerson)
         while i < len(meetings):
-            time = meetings[i][2]
-            people = set()
-            while i < len(meetings) and meetings[i][2] == time:
+            people, time = set(), meetings[i][2]
+            # All meetings at the same time are processed together as one time slice.
+            # During this time slice, the secret can spread freely within the connected
+            # components formed by these meetings.
+            # Afterward, only people connected to person 0 (i.e., those who already had
+            # access to the secret before this time slice) keep their connections;
+            # all others are reset to prevent the secret from leaking across time.
+            while i < len(meetings) and time == meetings[i][2]:
                 x, y, _ = meetings[i]
-                union(x, y)
-                people.add(x)
-                people.add(y)
-                i += 1
+                union(x, y); i+=1
+                people.add(x); people.add(y)
             for p in people:
-                if find(p) != find(0):
-                    parent[p] = p  # reset
+                if find(p) != find(0): parent[p] = p
         return [i for i in range(n) if find(i) == find(0)]
-
