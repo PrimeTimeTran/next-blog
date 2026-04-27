484955215b394b928249aeccc5c7980de0c3e147
PrimeTimeTran <primetimetran@gmail.com>
2026-04-26 20:23:44 -0400

    Fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   experimental: {
-    optimizeUniversalDefaults: true,
+    // optimizeUniversalDefaults: true,
   },
   content: [
     './pages/**/*.js',
