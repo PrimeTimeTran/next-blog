9edd58c5180b2d5aaaa2de5ba305274304a0fdbd
PrimeTimeTran <primetimetran@gmail.com>
2026-04-28 17:40:08 -0400

    KB pipeline working

--- pages/kb/[...slug].js
+++ pages/kb/[...slug].js
@@ -1,81 +1,36 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
-
-function serialize(obj) {
-  return JSON.parse(JSON.stringify(obj))
-}
-function clean(obj) {
-  return JSON.parse(JSON.stringify(obj, (_, v) => (v === undefined ? null : v)))
-}
-
-/* -----------------------
- * STATIC PATHS
- * ---------------------- */
+import { getAllKbSlugs } from '@/lib/content/kb'
+import { resolveKbItem } from '@/lib/content/kb/resolveKbItem'
 
 export async function getStaticPaths() {
-  // const { getPublishedFiles } = await import('@/lib/mdx')
-  // const { toSlug, normalizeSlug } = await import('@/lib/server/files')
-  // // const { normalizeSlug } = await import('@/lib/server/files')
-  // const kbFiles = getPublishedFiles('kb')
-  // const paths = kbFiles.map((file) => {
-  //   const slug = normalizeSlug(file)
-  //   return {
-  //     params: {
-  //       slug: slug.split('/'),
-  //     },
-  //   }
-  // })
-  // return {
-  //   paths: paths,
-  //   fallback: false,
-  // }
+  const slugs = await getAllKbSlugs()
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug.split('/'),
+      },
+    })),
+    fallback: false,
+  }
 }
 
-/* -----------------------
- * STATIC PROPS
- * ---------------------- */
-export async function getStaticProps({ params }) {
-  // // const { safeMath } = await import('@/lib/mdx/safeMath')
-  // const { toSlug, normalizeSlugFromSlug } = await import('@/lib/server/files')
-  // const { buildKbTree, getFileBySlug, getAllFilesFrontMatter } = await import('@/lib/mdx')
-  // const slugArray = Array.isArray(params.slug)
-  //   ? params.slug
-  //   : typeof params.slug === 'string'
-  //   ? params.slug.split('/')
-  //   : []
-  // const slug = slugArray.join('/')
-  // const safeSlug = normalizeSlugFromSlug(slug)
-  // const rawKB = await getAllFilesFrontMatter('kb')
-  // const allKB = rawKB
-  //   .filter(Boolean)
-  //   .filter((item) => item.filePath)
-  //   .map((item) => ({
-  //     ...item,
-  //     slug: normalizeSlugFromSlug(toSlug(item.filePath)),
-  //   }))
-  //   .filter((item) => item.slug)
-  // const sidebarData = buildKbTree(allKB)
-  // const index = allKB.findIndex((item) => item.slug === safeSlug)
-  // let kbItem = await getFileBySlug('kb', safeSlug)
-  // // kbItem = safeMath(kbItem)
-  // if (!kbItem) {
-  //   return { notFound: true }
-  // }
-  // const prev = index >= 0 ? allKB[index + 1] || null : null
-  // const next = index >= 0 ? allKB[index - 1] || null : null
-  // return {
-  //   props: {
-  //     kbItem: clean(kbItem),
-  //     prev: serialize(prev),
-  //     next: serialize(next),
-  //     sidebarData: serialize(sidebarData),
-  //   },
-  // }
+export async function getStaticProps(ctx) {
+  const kbItem = await resolveKbItem(ctx.params.slug)
+
+  if (!kbItem) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      kbItem,
+    },
+  }
 }
 
-/* -----------------------
- * PAGE
- * ---------------------- */
 export default function Page({ kbItem, sidebarData, ...rest }) {
+  console.log({ kbItem })
   if (!kbItem) return null
 
   const layout = kbItem?.frontMatter?.layout || 'KBLayout'
